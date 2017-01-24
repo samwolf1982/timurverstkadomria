eb116e571dbb2986b371c23ece45e05ac4800700
samwolf1982 <biryukwolf@yahoo.com>
2017-01-24 03:40:30 +0200

    novostroiki_podrobno ok

--- novostroiki_podrobno.php
+++ novostroiki_podrobno.php
@@ -3,6 +3,15 @@ include_once 'includes/header.php';
 include_once 'includes/carusele.php';
  include_once 'includes/fullscreenfilter.php';
 ?>
+
+
+
+        <!-- vertical carusele -->
+		  <link href="css/thumbs2.css" rel="stylesheet" />
+    <link href="css/thumbnail-slider.css" rel="stylesheet" type="text/css" />
+    <script src="libs/thumbnail-slider.js" type="text/javascript"></script>
+
+
 <style type="text/css">
 	<?php
 	include_once 'css/novostroiki_podrobno.css';
@@ -18,10 +27,7 @@ include_once 'includes/carusele.php';
 </script>
 
 
-           <!-- vertical carusele -->
-		  <link href="css/thumbs2.css" rel="stylesheet" />
-    <link href="css/thumbnail-slider.css" rel="stylesheet" type="text/css" />
-    <script src="libs/thumbnail-slider.js" type="text/javascript"></script>
+   
 
 <!-- <script type="text/javascript" src="libs/adg-galery/jquery.ad-gallery.js"></script> -->
 <!-- <div class="container">
@@ -113,7 +119,7 @@ include_once 'includes/carusele.php';
 	 <h1 class="title_room">Продажа двухкомнатной квартиры в Одессе по ул. Балковская</h1>	
 	 </div>	 
 
-<div class="col-sm-12">
+<div class="col-sm-12" style="    padding-bottom: 1em;">
 		<div class="col-sm-6"><a href="" class="clear_link"><i class="icon-like "></i>Добавить в избранное</a></div>
 	
 
@@ -128,7 +134,7 @@ include_once 'includes/carusele.php';
 <div class="container wrap_content">
 
 	<div class="row">
-		<div  class="col-md-9" style="padding-right: 0; ">
+		<div  class="col-sm-12 col-md-12 col-lg-9" style="padding-right: 0; padding-left: 0; ">
 
 			<div class="wrap_catalog  "> 
 
@@ -159,9 +165,9 @@ include_once 'includes/carusele.php';
     <div class="row">
 		<div class="col-md-9 col-lg-7 nodecor">
 			<!-- artigo em destaque -->
-			<div class="featured-article">
+			<div class="featured-article main_image">
 				<a href="#">
-					<img src="/img/verstka/img2-500x350.jpg" alt="" class="thumb">
+					<img src="http://placehold.it/350x150" alt="" class="thumb">
 				</a>
 <!-- 				<div class="block-title">
 					<h2>Lorem ipsum dolor asit amet</h2>
@@ -209,6 +215,71 @@ include_once 'includes/carusele.php';
 	</div>
 
 
+		<div class="col-sm-12">
+			<h2 class=""><a href="" class="clear_link"> Новострои Одессы ЖК 'Острова'</a></h2>
+		</div>
+		<div class="col-sm-12" style="text-indent: 0.5em;">
+			<div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam voluptas dignissimos ut corrupti dolores, maiores provident a debitis ipsam deserunt harum cumque hic unde, tempore accusamus aut similique. Qui, ducimus.</div>
+			<div>Natus eius aperiam commodi, rem dolore perspiciatis minima ducimus! Similique repellendus voluptatibus eligendi reiciendis omnis suscipit cupiditate sint possimus sapiente ratione consectetur explicabo impedit deleniti dignissimos porro non, molestias vitae.</div>
+			<div>Aperiam officia consequatur enim, totam, est velit odio officiis excepturi optio illum magnam illo distinctio culpa porro! Eaque dolores dolorum aspernatur consequatur beatae accusantium, fugiat. Quam laboriosam fugit hic alias.</div>
+			<div>Nisi delectus itaque ullam veniam doloremque eum repellat labore non et rerum inventore molestias, voluptatibus reiciendis, maiores cumque laborum in fugiat harum provident iure, commodi? Ut quam consectetur alias eum.</div>
+			<div>Eaque ipsum animi corporis magni ducimus rerum quos, distinctio placeat saepe tenetur, ratione eos similique? Veniam iste reprehenderit sapiente, perspiciatis cupiditate sit consequatur quasi quas repellendus neque vero quia atque!</div>
+			<div>Nemo ullam ipsam nulla. Rerum voluptate dolor temporibus repellat officiis ipsum nihil minus. Minus saepe voluptate, laborum similique id, autem eveniet totam quasi architecto deleniti perferendis. Consequatur harum, nesciunt excepturi.</div>
+ <br>
+			 <h4 class="some_info">
+				Мадагаскар 1 дом сдан, Мадагаскар 2 дом сдан
+			</h4>
+
+		</div>
+           
+
+           <div class="col-sm-6">
+       <br>
+           <p class="broni_info some_info">
+Отдел бронирования
+    </p>    	
+  <table class="table">
+
+    <tbody>
+      <tr>
+        <td>Адресс</td>
+        <td>Minus saepe voluptate, laborum similique</td>
+        
+      </tr>
+      <tr>
+        <td>Телефон</td>
+        <td>
+                <div class="tel">1234567989</div>
+                                <div class="tel">1234567989</div>
+                                                <div class="tel">1234567989</div>
+                                                                <div class="tel">1234567989</div>
+        </td>   
+      </tr>
+
+       <tr>
+        <td>E-mail</td>
+        <td>demo@sobcak.com</td>   
+      </tr>
+  
+
+    </tbody>
+  </table>
+
+           </div>
+           <div class="col-sm-6">
+           	
+ <div class="wrap_btn">
+           	<div class="btn btn-block fill_button">
+
+	 <span class="buy_text">Получить подробную консультацию у нашего специалиста</span>		
+	 </div>
+	 </div>
+           </div>
+
+          
+	
+
+
 
 
 
@@ -226,7 +297,7 @@ include_once 'includes/carusele.php';
 
 
 		<!-- side bar -->
-		<div class="col-md-3 text-center" style="    padding: 0;">
+		<div class="col-sm-12 col-md-12 col-lg-3 text-center" style="    padding: 0;">
 			<div class="row">
 
 
@@ -239,7 +310,7 @@ include_once 'includes/carusele.php';
 
 
 					<div class="news-box wrap_gogle_map news-title-obj">
-					<div class="title_ibj">Обекты на карте</div>
+					<div class="title_ibj">На карте</div>
 					
 					<div>&nbsp;</div>
 						<div id="map"></div>
